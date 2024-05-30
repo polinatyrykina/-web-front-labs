c1831d6e4bd67c84e6728ec6a663e848489e44a8
Тырыкина Полина <polinatyrykina@gmail.com>
2024-05-30 16:12:35 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -2,7 +2,9 @@ function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     let seconds = today.getSeconds();
+    let totalSeconds = today.getHours() * 3600 + today.getMinutes() * 60 + seconds;
     document.getElementById('time').innerHTML = currentTime;
     document.getElementById('seconds').innerHTML = seconds;
+    document.getElementById('time1').style.transform = `rotate(${totalSeconds * 6}deg)`;
 }
-setInterval(showTime, 1000)
+setInterval(showTime, 1000);
